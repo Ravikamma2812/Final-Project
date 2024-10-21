36826a8a6b613aa28ac4ed9aff9d9dc0f947c04e
Ravi Kamma <143317296+Ravikamma2812@users.noreply.github.com>
2024-10-21 18:24:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,79 +1,43 @@
-import streamlit as st
 import pandas as pd
 import numpy as np
+import sklearn
+import streamlit as st
 import joblib
-from sklearn.ensemble import RandomForestClassifier
-from prediction import get_prediction, ordinal_encoder
+import shap
+import matplotlib
+from IPython import get_ipython
+from PIL import Image
 
-model = joblib.load(r'Model/Baseline_RandomForest_final.joblib')
+# load the encoder and model object
+model = joblib.load("rta_model_deploy3.joblib")
+encoder = joblib.load("ordinal_encoder2.joblib")
+st.set_option('deprecation.showPyplotGlobalUse', False)
 
-st.set_page_config(page_title="Accident Severity Prediction App", page_icon="🚧", layout="wide")
+# 1: serious injury, 2: Slight injury, 0: Fatal Injury
 
-# creating an option list for the drop down menus
+st.set_page_config(page_title="Accident Severity Prediction App",
+                page_icon="🚧", layout="wide")
 
+#creating option list for dropdown menu
 options_day = ['Sunday', "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
 options_age = ['18-30', '31-50', 'Over 51', 'Unknown', 'Under 18']
+options_types_collision = ['Vehicle with vehicle collision','Collision with roadside objects',
+                           'Collision with pedestrians','Rollover','Collision with animals',
+                           'Unknown','Collision with roadside-parked vehicles','Fall from vehicles',
+                           'Other','With Train']
 
+options_sex = ['Male','Female','Unknown']
+
+options_education_level = ['Junior high school','Elementary school','High school',
+                           'Unknown','Above high school','Writing & reading','Illiterate']
+
+options_services_year = ['Unknown','2-5yrs','Above 10yr','5-10yrs','1-2yr','Below 1yr']
 options_acc_area = ['Other', 'Office areas', 'Residential areas', ' Church areas',
        ' Industrial areas', 'School areas', '  Recreational areas',
        ' Outside rural areas', ' Hospital areas', '  Market areas',
        'Rural village areas', 'Unknown', 'Rural village areasOffice areas',
        'Recreational areas']
-       
-options_cause = ['No distancing', 'Changing lane to the right',
-       'Changing lane to the left', 'Driving carelessly',
-       'No priority to vehicle', 'Moving Backward',
-       'No priority to pedestrian', 'Other', 'Overtaking',
-       'Driving under the influence of drugs', 'Driving to the left',
-       'Getting off the vehicle improperly', 'Driving at high speed',
-       'Overturning', 'Turnover', 'Overspeed', 'Overloading', 'Drunk driving',
-       'Unknown', 'Improper parking']
-options_vehicle_type = ['Automobile', 'Lorry (41-100Q)', 'Other', 'Pick up upto 10Q',
-       'Public (12 seats)', 'Stationwagen', 'Lorry (11-40Q)',
-       'Public (13-45 seats)', 'Public (> 45 seats)', 'Long lorry', 'Taxi',
-       'Motorcycle', 'Special vehicle', 'Ridden horse', 'Turbo', 'Bajaj', 'Bicycle']
-options_driver_exp = ['5-10yr', '2-5yr', 'Above 10yr', '1-2yr', 'Below 1yr', 'No Licence', 'unknown']
-options_lanes = ['Two-way (divided with broken lines road marking)', 'Undivided Two way',
-       'other', 'Double carriageway (median)', 'One way',
-       'Two-way (divided with solid lines road marking)', 'Unknown']
 
-features = ['hour','day_of_week','casualties','accident_cause','vehicles_involved','vehicle_type','driver_age','accident_area','driving_experience','lanes']
-
-st.markdown("<h1 style='text-align: center;'>Accident Severity Prediction App 🚧</h1>", unsafe_allow_html=True)
-
-def main():
-    with st.form('prediction_form'):
-        st.subheader("Enter the input for following features:")
-
-        hour = st.slider("Hour of Accident: ", 0, 23, value=0, format="%d")
-        day_of_week = st.selectbox("Select Day of the Week: ", options=options_day)
-        casualties = st.slider("No of Casualties: ", 1,8, value=0, format="%d")
-        accident_cause = st.selectbox("Cause of Accident: ", options=options_cause)
-        vehicles_involved = st.slider("Vehicles Involved: ", 1, 7, value=0, format="%d")
-        vehicle_type = st.selectbox("Vehicle Type: ", options=options_vehicle_type)
-        driver_age = st.selectbox("Driver Age: ", options=options_age)
-        accident_area = st.selectbox("Accident Area", options=options_acc_area)
-        driving_experience = st.selectbox("Driving Experience: ", options=options_driver_exp)
-        lanes = st.selectbox("Lane of Accident: ", options=options_lanes)
-        
-        submit = st.form_submit_button("Predict")
-
-
-    if submit:
-        day_of_week = ordinal_encoder(day_of_week, options_day)
-        accident_cause = ordinal_encoder(accident_cause, options_cause)
-        vehicle_type = ordinal_encoder(vehicle_type, options_vehicle_type)
-        driver_age =  ordinal_encoder(driver_age, options_age)
-        accident_area =  ordinal_encoder(accident_area, options_acc_area)
-        driving_experience = ordinal_encoder(driving_experience, options_driver_exp) 
-        lanes = ordinal_encoder(lanes, options_lanes)
-
-
-        data = np.array([hour,day_of_week,casualties,accident_cause,vehicles_involved, vehicle_type,driver_age,accident_area,driving_experience,lanes]).reshape(1,-1)
-
-        pred = get_prediction(data=data, model=model)
-
-        st.write(f"The predicted severity is:  {pred[0]}")
-
-if __name__ == '__main__':
-    main()
+# features list
+features = ['Number_of_vehicles_involved','Number_of_casualties','Hour_of_Day','Type_of_collision','Age_band_of_driver','Sex_of_driver',
+       'Educational_level','Service_year_of_vehicle','Day_of_week','Area_accident_occured']
