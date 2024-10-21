3048d9ac66ec7de165e509911936e793d14415db
Ravi Kamma <143317296+Ravikamma2812@users.noreply.github.com>
2024-10-21 18:28:22 +0530

    Update prediction.py

--- prediction.py
+++ prediction.py
@@ -1,16 +1,86 @@
-import joblib
-import numpy as np
+import pandas as pd 
+
+# using pandas read_csv function to load the dataset 
+df = pd.read_csv("RTA Dataset.csv") 
+print(df.head())
+df.info()
+df.isnull().sum()
+print(df['Accident_severity'].value_counts())
+df['Accident_severity'].value_counts().plot(kind='bar')
+df['Time'] = pd.to_datetime(df['Time'])
+
+# Extrating 'Hour_of_Day' feature from the Time column
+new_df = df.copy()
+new_df['Hour_of_Day'] = new_df['Time'].dt.hour
+n_df = new_df.drop('Time', axis=1)
+n_df.head()
+feature_df['Service_year_of_vehicle'] = feature_df['Service_year_of_vehicle'].fillna('Unknown')
+feature_df['Types_of_Junction'] = feature_df['Types_of_Junction'].fillna('Unknown')
+feature_df['Area_accident_occured'] = feature_df['Area_accident_occured'].fillna('Unknown')
+feature_df['Driving_experience'] = feature_df['Driving_experience'].fillna('unknown')
+feature_df['Type_of_vehicle'] = feature_df['Type_of_vehicle'].fillna('Other')
+feature_df['Vehicle_driver_relation'] = feature_df['Vehicle_driver_relation'].fillna('Unknown')
+feature_df['Educational_level'] = feature_df['Educational_level'].fillna('Unknown')
+feature_df['Type_of_collision'] = feature_df['Type_of_collision'].fillna('Unknown')
+
+# features information
+feature_df.info()
+features = ['Day_of_week','Number_of_vehicles_involved','Number_of_casualties','Area_accident_occured',
+           'Types_of_Junction','Age_band_of_driver','Sex_of_driver','Educational_level',
+           'Vehicle_driver_relation','Type_of_vehicle','Driving_experience','Service_year_of_vehicle','Type_of_collision',
+           'Sex_of_casualty','Age_band_of_casualty','Cause_of_accident','Hour_of_Day']
+
+# setting input features X and target y 
+X = feature_df[features]  # here features are selected from 'object' datatype
+y = n_df['Accident_severity']
+
+# we will use pandas get_dummies method for on-hot encoding
+encoded_df = pd.get_dummies(X, drop_first=True)
+encoded_df.shape
+from sklearn.preprocessing import LabelEncoder
+
+# create labelencoder object
+lb = LabelEncoder()
+lb.fit(y)
+y_encoded = lb.transform(y)
+print("Encoded labels:",lb.classes_)
+y_en = pd.Series(y_encoded)
+from sklearn.feature_selection import SelectKBest, chi2
+fs = SelectKBest(chi2, k=50)
+X_new = fs.fit_transform(encoded_df, y_en)
+
+# Take the selected features
+cols = fs.get_feature_names_out()
+
+# convert selected features into dataframe
+fs_df = pd.DataFrame(X_new, columns=cols)
+# importing the SMOTENC object from imblearn library 
+from imblearn.over_sampling import SMOTENC
+
+# categorical features for SMOTENC technique for categorical features
+n_cat_index = np.array(range(3,50))
+
+# creating smote object with SMOTENC class
+smote = SMOTENC(categorical_features=n_cat_index, random_state=42, n_jobs=True)
+X_n, y_n = smote.fit_resample(fs_df,y_en)
+
+# print the shape of new upsampled dataset
+X_n.shape, y_n.shape
+from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import confusion_matrix, classification_report, f1_score
 
-def ordinal_encoder(input_val, feats): 
-    feat_val = list(1+np.arange(len(feats)))
-    feat_key = feats
-    feat_dict = dict(zip(feat_key, feat_val))
-    value = feat_dict[input_val]
-    return value
+# train and test split and building baseline model to predict target features
+X_trn, X_tst, y_trn, y_tst = train_test_split(X_n, y_n, test_size=0.2, random_state=42)
 
-def get_prediction(data,model):
-    """
-    Predict the class of a given data point.
-    """
-    return model.predict(data)
+# modelling using random forest baseline
+rf = RandomForestClassifier(n_estimators=800, max_depth=20, random_state=42)
+rf.fit(X_trn, y_trn)
+
+# predicting on test data
+predics = rf.predict(X_tst)
+
+# train score 
+rf.score(X_trn, y_trn)
+classif_re = classification_report(y_tst,predics)
+print(classif_re)
